a6a181715d4838d2bfac56e5ea458e98141dba05
Battlebee12 <Sarab4space@gmail.com>
2023-12-18 01:44:53 -0800

    collisins

---
+++ ApSar/Assets/Scripts/collision.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+
+{
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   private void OnTriggerEnter2D(Collider2D other)
+   
+    {
+        Debug.Log("collison detected");
+        // Check if the collided object has the "Player" tag
+        if (other.CompareTag("main_character"))
+        {
+            // Perform actions specific to the player (optional)
+            Debug.Log("Fruit touched the player!");
+
+            // Destroy the fruit GameObject when it collides with the player
+            Destroy(gameObject);
+        }
+        if (other.CompareTag("floor")){
+             Destroy(gameObject);
+
+        }
+    }
+}
